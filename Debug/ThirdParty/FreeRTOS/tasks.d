e3cc55b8e8414a1a7758223a4d4190391ffa0896
ArjunRavi009 <139131717+ArjunRavi009@users.noreply.github.com>
2025-12-11 23:26:45 +0530

    Integrated FreeRTOS on STM32F446R MCU project

---
+++ Debug/ThirdParty/FreeRTOS/tasks.d
@@ -0,0 +1,25 @@
+ThirdParty/FreeRTOS/tasks.o: ../ThirdParty/FreeRTOS/tasks.c \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/list.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/timers.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/stack_macros.h
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/list.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/timers.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/AR59/PROJECT_Documents/POCs/GIT/STM32_RTOS_Integration/RTOS_Integration/ThirdParty/FreeRTOS/include/stack_macros.h:
